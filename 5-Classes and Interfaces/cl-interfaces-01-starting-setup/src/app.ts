b128844c206d600228c2155a8567233c9db2e209
Lucas Arthur <lucasmella16@gmail.com>
2023-12-21 14:21:40 -0300

    feito a sessao de recursos novo de js/ts e comecado a sessao de classes e interfaces

---
+++ 5-Classes and Interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -0,0 +1,36 @@
+class Department {
+    name: string = "DEFAULT";
+
+    private employees: string[] = [];
+
+    constructor(n: string){
+        this.name = n;
+    }
+
+    describe(this: Department) {
+        console.log("Department: " + this.name);
+    }
+
+    addEmployee(employee: string){
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+
+}
+
+const accounting = new Department("Accounting");
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+accounting.addEmployee("Anna");
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: "s", describe: accounting.describe };
+// accountingCopy.describe();
